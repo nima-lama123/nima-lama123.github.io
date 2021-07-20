18b1ae4653942823ec3ae77f4c67a5b42f556828
nima-lama123 <ninalama560@gmail.com>
2021-07-20 17:59:54 +0545

    first

---
+++ app.js
@@ -0,0 +1,29 @@
+let color= document.getElementById("color");
+
+let btn = document.getElementById("btn");
+
+const hex=[0,1,2,3,4,5,6,7,8,9,"A","B","C","D","E","F"];
+
+
+
+btn.addEventListener("click",function(){
+
+    let hexColor ="#";
+
+    for(let i=0; i<6; i++){
+
+        hexColor += hex[getrandomNumber()];
+
+    }
+
+  color.textContent = hexColor;
+
+document.body.style.backgroundColor = hexColor;
+
+})
+
+function getrandomNumber(){
+
+    return Math.floor(Math.random() * hex.length);
+
+}
